990120aeef7990ac4598089ab9da2936c5dde7b7
Leticia Raphaele <90052051+LeticiaRaphaele@users.noreply.github.com>
2024-07-07 18:47:59 -0300

    tratamento de exceções

--- src/main/java/com/lms/teste/Controller/UserController.java
+++ src/main/java/com/lms/teste/Controller/UserController.java
@@ -21,47 +21,49 @@ import com.lms.teste.Models.TokenRequest;
 import com.lms.teste.Models.User;
 import com.lms.teste.Service.UserService;
 
-
-
 @RestController
+
 @RequestMapping("/api/users")
-public class UserController{
+public class UserController {
+
     @Autowired
     private UserService service;
 
     private Auth tocken = new Auth();
 
     @GetMapping("/")
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return service.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id){
+    public User getUser(@PathVariable Long id) {
         return service.getUserById(id);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id){
+    public void deleteUser(@PathVariable Long id) {
         service.deleteUser(id);
     }
-    
+
     @PostMapping("/")
-    public User createUser(@RequestBody User user){
+    public User createUser(@RequestBody User user) {
         return service.createUser(user);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         // Busque o usuário no banco de dados pelo e-mail
-        try{
+        try {
             User usuarioBanco = service.findByEmail(loginRequest.getEmail());
-            
+
             // Verifique se o usuário existe e se a senha é correta
-            if (usuarioBanco != null && usuarioBanco.getSenha().equals(loginRequest.getSenha())) {
-                
-                String token = tocken.generateToken("" + usuarioBanco.getId(), ""+usuarioBanco.getPapel(),usuarioBanco.getNome());
-                
+            if (usuarioBanco != null &&
+                    usuarioBanco.getSenha().equals(loginRequest.getSenha())) {
+
+                String token = tocken.generateToken("" + usuarioBanco.getId(),
+                        "" + usuarioBanco.getPapel(), usuarioBanco.getNome());
+
                 // Retorne o token no corpo da resposta
                 Map<String, String> response = new HashMap<>();
                 response.put("token", token);
@@ -78,28 +80,27 @@ public class UserController{
     @PostMapping("/validateToken")
     public ResponseEntity<?> validateToken(@RequestBody TokenRequest tokenRequest) {
         try {
-            if(!tokenRequest.getToken().isEmpty()){
-                DecodedJWT jwt  = tocken.decodeToken(tokenRequest);
+            if (!tokenRequest.getToken().isEmpty()) {
+                DecodedJWT jwt = tocken.decodeToken(tokenRequest);
 
                 // Create a response map
                 Map<String, Object> response = new HashMap<>();
                 response.put("id", Long.parseLong(jwt.getSubject()));
                 response.put("nome", jwt.getClaim("name").asString());
                 response.put("papel", jwt.getClaim("role").asString());
-    
+
                 return ResponseEntity.ok(response);
-            }else{
+            } else {
                 return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
             }
-            
 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
         }
     }
 
-    //(X)colocar try catch 
-    //(x)colocar response para erros
-    //()fazer teste de unidade 
-    //()fazer teste de integra
+    // (X)colocar try catch
+    // (x)colocar response para erros
+    // ()fazer teste de unidade
+    // ()fazer teste de integra
 }
--- src/main/java/com/lms/teste/Repository/UserRepository.java
+++ src/main/java/com/lms/teste/Repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.lms.teste.Repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lms.teste.Models.User;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
