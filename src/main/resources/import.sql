6507343167bcc0c02fb78f9f83376baac3cb03ff
Ricardo Rodrigues <ricardor662@gmail.com>
2024-07-16 23:46:34 -0300

    implement test update atividade and fix bugs

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,3 +9,5 @@ INSERT INTO users (nome, senha, email, ativo, papel) VALUES ('Linus Torvalds', '
 INSERT INTO users (nome, senha, email, ativo, papel) VALUES ('Margaret Hamilton', 'password8', 'margaret.hamilton@example.com', true, 'ADMIN');
 INSERT INTO users (nome, senha, email, ativo, papel) VALUES ('James Gosling', 'password9', 'james.gosling@example.com', true, 'USER');
 INSERT INTO users (nome, senha, email, ativo, papel) VALUES ('Guido van Rossum', 'password10', 'guido.vanrossum@example.com', true, 'USER');
+INSERT INTO atividades (titulo, descricao, data_inicio, data_fim, grupo, nota_maxima) VALUES ('Atividade', 'Atividade teste', '2024-07-16T23:17:23.9450241', '2024-07-16T23:17:23.9450241', false, 10);
+INSERT INTO atividades (titulo, descricao, data_inicio, data_fim, grupo, nota_maxima) VALUES ('Nova Atividade', 'Atividade teste', '2024-07-16T23:17:23.9450241', '2024-07-16T23:17:23.9450241', true, 8);
